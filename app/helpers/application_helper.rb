b286bde70b3f4617f885691457cdbc0f6f519fed
Prasanna Sundar <prasanna.s@freshdesk.com>
2015-01-06 12:23:16 +0530

    changed app helper and model user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def provide_title(name="")
+		if name==""
+			"Todo"
+		else
+			"#{name} | Todo"
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	has_secure_password
+	validates :username, length: {in: 6..20}, presence: true
+	validates :password, length: {in: 6..20}, presence: true
+	validates :email, format: {with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, presence: true
 end
